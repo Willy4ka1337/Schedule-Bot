7a77b210420df99e03b3ab7fa275b856279736b7
Willy4ka <55584863+Willy4ka1337@users.noreply.github.com>
2025-01-18 12:44:45 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const port = process.env.PORT || 4000;
 const { PythonShell } = require('python-shell');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('<h1>This is a web service for a Schedule bot</h1><br>Developer: <a href="https://t.me/SchizophreniaCP1251">Willy4ka</a>')
 })
-  
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
-
 
 PythonShell.run('main.py', null, (err, results) => {
     if (err) throw err;
